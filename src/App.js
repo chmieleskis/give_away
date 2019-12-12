32fc66687b1f12495f9716c763623353e2127d5b
Michal Chmielewski <m.chmielewski@tut.by>
2019-12-12 03:24:27 +0100

    app config

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus asperiores cupiditate debitis ducimus iste modi neque nostrum, obcaecati placeat, quae repellat, ut voluptas. Error inventore ipsum odit soluta tempore!</p>
+        </div>
+    )
+  }
+}
+
+export default App;
