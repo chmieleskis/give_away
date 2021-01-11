4480478bd7d60a10d238b917829d1a482ca7c3ac
Michal Chmielewski <m.chmielewski@tut.by>
2021-01-11 20:02:31 +0100

    Updates

---
+++ precache-manifest.b2ab11d127692754d369dcf70d892893.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6178dc0de226ea4c58687f47613b4454",
+    "url": "/give_away/index.html"
+  },
+  {
+    "revision": "74c2937063986dafc145",
+    "url": "/give_away/static/css/main.27a5e41e.chunk.css"
+  },
+  {
+    "revision": "b9b1269b1437d89cca0b",
+    "url": "/give_away/static/js/2.e095ec30.chunk.js"
+  },
+  {
+    "revision": "b4f9955a46235ab5d9673fafd3d3f6c5",
+    "url": "/give_away/static/js/2.e095ec30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74c2937063986dafc145",
+    "url": "/give_away/static/js/main.83ae9eb2.chunk.js"
+  },
+  {
+    "revision": "4ec8b32cd4984a3f4a4b",
+    "url": "/give_away/static/js/runtime-main.9337108e.js"
+  },
+  {
+    "revision": "7db36abd712c228ea96e6206440a9eb3",
+    "url": "/give_away/static/media/3 Columns Background.7db36abd.png"
+  },
+  {
+    "revision": "f3e1cf8fa3495d76fe9ca243cd75c869",
+    "url": "/give_away/static/media/Background Image.f3e1cf8f.png"
+  },
+  {
+    "revision": "6baf5950179f12f109e487a7ec03faac",
+    "url": "/give_away/static/media/Decoration.6baf5950.svg"
+  },
+  {
+    "revision": "ef2519d20c70fe42a96e8c744772c6ef",
+    "url": "/give_away/static/media/Hero Image.ef2519d2.png"
+  },
+  {
+    "revision": "8492ed522d9a52287675b55a96a4ded4",
+    "url": "/give_away/static/media/People.8492ed52.png"
+  },
+  {
+    "revision": "d87cadf24b263287f4d33024c61c02e5",
+    "url": "/give_away/static/media/Signature.d87cadf2.svg"
+  }
+]);
